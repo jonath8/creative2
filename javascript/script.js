cca172fb3739622218912b3f450c5af1b24413ca
jonath8 <jonathan.brent.nelson@gmail.com>
2021-10-16 17:10:44 -0600

    Initial script attempt

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,39 @@
-document.getElementById("gameSubmit").addEventListener("click", function(event) {
-  event.preventDefault();
-  const value = document.getElementById("gameInput").value;
-  if (value === "")
-  return;
-  const url = "https://acnhapi.com/v1/";
+function onClick(e) {
+  debugger
+  e.preventDefault();
+  // get form values
+  let elephantInput = document.getElementById('elephantInput').value;
+  let s = document.getElementById('selector');
+  let type = s.options[s.selectedIndex].value;
+
+
+  // setup URL
+  //let url = "http://numberapi.com/" + number + "/" + "type" + "?json";
+  let url = "https://elephant-api.herokuapp.com/";
+  if (type === "random") {
+    url += "elephants/random";
+  } elseif (type === "name") {
+    url += "elephants/name/" + elephantInput;
+  } elseif (type === "sex") {
+    url += "elephants/sex/" + elephantInput;
+  } elseif (type === "species") {
+    url += "species/" + elephantInput;
+  } else {
+    url += "elephants";
+  }
+  // call API
   fetch(url)
     .then(function(response) {
+      // make sure the request was successful
+      if (response.status != 200) {
+        return {
+          text: "Error calling the Numbers API service: " + response.statusText
+        }
+      }
       return response.json();
     }).then(function(json) {
-      let results = "";
+      console.log("this functioned")
     });
+}
 
-
-  document.getElementById("gameResults").innerHTML = results;
-  });
+document.getElementById('elephants').addEventListener('click', onClick);
